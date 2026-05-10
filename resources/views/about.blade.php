5f4527f3d24a7cab375effaf9a15d64531ae91b9
hadeelizzat <2205250127std@gtc.edu.ps>
2026-05-10 13:19:29 +0300

    update

---
+++ resources/views/about.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+<h1>Hello, {{$name}}</h1>
+<form action="about" method="post" >
+    @csrf
+    <input type="text" name="name" id="name"><br>
+    <br>
+    <select>
+        @foreach ($departments as $key=> $department)
+      <option value={{$key}}>{{$department}}</option>
+        @endforeach
+
+
+    </select>
+    <br>
+    <br>
+    <input type="submit" value="submit">
+</form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about',function(){
+    $name='Hadeel';
+     $departments=[
+    '1'=>'Tichnical',
+    '2'=>'Financial',
+    '3'=>'sales'
+ ];
+//return view(view:'about')->with('name',$name);
+//return view('about',['name'=> $name]);
+return view ('about',compact('name','departments'));
+});
+Route::post('/about',function(){
+ $name = $_POST['name'];
+ $departments=[
+    '1'=>'Tichnical',
+    '2'=>'Financial',
+    '3'=>'sales'
+ ];
+ return view('about',compact('name','departments'));
+});
+
