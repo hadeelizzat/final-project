bc3de5b9e5a156689e54867e8c3558a475add72f
hadeelizzat <2205250127std@gtc.edu.ps>
2026-06-01 12:58:51 +0300

    update3

---
+++ resources/views/users.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app2')
+@section('content')
+<div class="card">
+     @if (isset($user))
+         <h5 class="card-header">Update user</h5>
+         <div class="card-body">
+
+             <form action="{{ url('updateUser') }}" method="POST">
+                 @csrf
+                 <input type="hidden" name="id" value="{{ $user->id }}">
+                 <div class="mb-3">
+                     <label for="user-name" class="form-label">User</label>
+                     <input type="text" name="name" class="form-control" id="user-name"
+                         value="{{ $user->name }}">
+                 </div>
+                 <div class="mb-3">
+                     <label for="user-email" class="form-label">Email</label>
+                     <input type="email" name="email" class="form-control" id="user-email"
+                         value="{{ $user->email }}">
+                 </div>
+                 <div class="mb-3">
+                     <label for="user-password" class="form-label">password</label>
+                     <input type="password" name="password" class="form-control" id="user-password"
+                         value="{{ $user->password }}">
+                 </div>
+                 <div>
+                     <button type="submit" class="btn btn-primary">
+                         Update user
+                     </button>
+                 </div>
+             </form>
+         </div>
+ </div>
+@else
+ <div class="card">
+
+     <h5 class="card-header">New user</h5>
+     <div class="card-body">
+
+         <form action="createUser" method="POST">
+             @csrf
+             <div class="mb-3">
+                 <label for="user-name" class="form-label">User</label>
+                 <input type="text" name="name" class="form-control" id="user-name" required>
+             </div>
+             <div class="mb-3">
+                 <label for="user-email" class="form-label">Email</label>
+                 <input type="email" name="email" class="form-control" id="user-email" required>
+             </div>
+             <div class="mb-3">
+                 <label for="user-password" class="form-label">password</label>
+                 <input type="password" name="password" class="form-control" id="user-password" required>
+             </div>
+             <button type="submit" class="btn btn-primary">
+                 Add user
+             </button>
+         </form>
+     </div>
+ </div>
+ @endif
+ <div class="card mt-4">
+     <table class="table">
+         <thead>
+             <tr>
+                 <th scope="col">User</th>
+                 <th scope="col">Email</th>
+                 <th scope="col">Password</th>
+                 <th scope="col">Actions</th>
+             </tr>
+         </thead>
+         <tbody>
+             @foreach ($users as $user)
+                 <tr>
+                     <td>{{ $user->name }}</td>
+                     <td>{{ $user->email }}</td>
+                     <td>{{ $user->password }}</td>
+                     <td>
+                         <form action="/deleteUser/{{ $user->id }}" method="POST" class="d-inline">
+                             @csrf
+                             <button type="submit" class="btn btn-danger">
+                                 Delete
+                             </button>
+                         </form>
+                         <form action="/editUser/{{ $user->id }}" method="POST" class="d-inline">
+                             @csrf
+                             <button type="submit" class="btn btn-info">
+                                 Edit
+                             </button>
+                         </form>
+                     </td>
+                 </tr>
+             @endforeach
+         </tbody>
+     </table>
+ </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,57 @@
 <?php
 
+use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about',function(){
-    $name='Hadeel';
-     $departments=[
-    '1'=>'Tichnical',
-    '2'=>'Financial',
-    '3'=>'sales'
- ];
-//return view(view:'about')->with('name',$name);
-//return view('about',['name'=> $name]);
-return view ('about',compact('name','departments'));
+Route::get('/about', function () {
+    $name = 'Hadeel';
+    $departments = [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'sales'
+    ];
+    //return view(view:'about')->with('name',$name);
+    //return view('about',['name'=> $name]);
+    return view('about', compact('name', 'departments'));
 });
-Route::post('/about',function(){
- $name = $_POST['name'];
- $departments=[
-    '1'=>'Tichnical',
-    '2'=>'Financial',
-    '3'=>'sales'
- ];
- return view('about',compact('name','departments'));
+Route::post('/about', function () {
+    $name = $_POST['name'];
+    $departments = [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'sales'
+    ];
+    return view('about', compact('name', 'departments'));
 });
-Route::get('tasks',function(){
-    $tasks= DB::table('tasks')->get();
-    return view('tasks',compact('tasks'));
-
+Route::get('tasks', function () {
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('tasks'));
 });
-Route::post('create',function(){
- $task_name= $_POST['name'];
-    DB::table('tasks')->insert(['name'=>$task_name]);
+Route::post('create', function () {
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert(['name' => $task_name]);
     return redirect()->back();
 });
-Route::post('delete/{id}',function($id){
-DB::table('tasks')->where('id',$id)->delete();
-return redirect()->back();
 
+Route::post('delete/{id}', function ($id) {
+    DB::table('tasks')->where('id', $id)->delete();
+    return redirect()->back();
 });
-Route::post('edit/{id}',function($id){
-    $task = DB::table('tasks')->where('id',$id)->first();
-    $tasks= DB::table('tasks')->get();
-    return view('tasks',compact('task','tasks'));
-
+Route::post('edit/{id}', function ($id) {
+    $task = DB::table('tasks')->where('id', $id)->first();
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('task', 'tasks'));
 });
-Route::post('update',function(){
-   $id= $_POST['id'];
-    DB::table('tasks')->where('id','=',$id)->update(['name'=>$_POST['name']]);
+Route::post('update', function () {
+    $id = $_POST['id'];
+    DB::table('tasks')->where('id', '=', $id)->update(['name' => $_POST['name']]);
     return redirect('tasks');
-
 });
-
-
-
+Route::get('users', [userController::class, 'index']);
+Route::post('createUser', [userController::class, 'createUser']);
+Route::post('deleteUser/{id}', [userController::class, 'deleteUser']);
+Route::post('editUser/{id}', [userController::class, 'editUser']);
+Route::post('updateUser', [userController::class, 'updateUser']);
